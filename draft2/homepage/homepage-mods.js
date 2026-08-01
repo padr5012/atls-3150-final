4108ae9efc99abd39e28bb16e01b0663f057457b
Hamburami <hape2715@colorado.edu>
2026-07-31 21:44:18 -0600

    added esc to close mobile nav menu

--- draft2/homepage/homepage-mods.js
+++ draft2/homepage/homepage-mods.js
@@ -2,4 +2,17 @@
 document.getElementById('pum-1412').style.display='none';
 document.getElementById('pum-1171').style.display='none';
 
+// Added a keyboard escape from the popup mobile menu
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape'){
+    const menu = document.querySelector('.zyth-modern-menu-content');
+    if (menu.getBoundingClientRect().height === 0) return; // if the menu is already closed, return.
+    // Otherwise vvvv
+    document.querySelector('.zyth-modern-menu-close').click(); // close the menu
+    document.querySelector('.zyth-mobile-menu-button').focus();
+  }
+});
+
+
+
 
